3375f4fb3d7042a71c24503094458e9d3ae11cf6
shysha <shyshasy@gmail.com>
2024-11-10 13:06:28 +0000

    Updated auth.js

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,34 +1,33 @@
 import { defineStore } from 'pinia';
-import axios from 'axios'; // Assurez-vous d'avoir installé axios
-
+import axios from 'axios'; 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     user: null,
     isAuthenticated: false,
   }),
   actions: {
-    // Méthode pour se connecter
+  
     login(user) {
       this.user = user;
       this.isAuthenticated = true;
       localStorage.setItem('token', user.token);
     },
-    // Méthode pour se déconnecter
+  
     logout() {
       this.user = null;
       this.isAuthenticated = false;
       localStorage.removeItem('token');
     },
-    // Méthode pour s'inscrire
+  
     async register(userData) {
       try {
         const response = await axios.post('/api/register', userData);
-        this.user = response.data.user; // En supposant que la réponse contient l'utilisateur
-        this.isAuthenticated = true; // Vous pouvez définir l'utilisateur comme authentifié
-        localStorage.setItem('token', response.data.token); // Stockez le token si disponible
+        this.user = response.data.user; 
+        this.isAuthenticated = true; 
+        localStorage.setItem('token', response.data.token); 
       } catch (error) {
         console.error('Erreur d\'inscription:', error.response ? error.response.data : error);
-        throw error; // Propager l'erreur pour la gérer dans le composant
+        throw error; 
       }
     },
   },
